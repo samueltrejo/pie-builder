018139cfa929fbe8bc7d5be065dff5f98d648d8a
Samuel Trejo <samuel.trejo@s.tcatnashville.edu>
2019-03-25 20:30:35 -0500

    added form

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hi')
+let ingredients = []
+
+const addIngredient = () => {
+  let domString = document.getElementById('ingredient').value
+  printToDom('ingredients-container', domString)
+  document.getElementById('ingredient').value = '';
+}
+
+const addEventListeners = () => {
+  document.getElementById('add-ingredient').addEventListener('click', addIngredient);
+}
+
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint
+}
+
+const init = () => {
+  addEventListeners()
+}
+
+init();
